5489571648aa30a7be15230bfc2264e17d3bf8d0
tjerk valentijn <tjerk.valentijn@gmail.com>
2016-06-09 20:21:48 +0200

    Update 2016-06-09T18:21:46.528Z

--- src/Index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import AppComponent from "./components/AppComponent";
-
-window.React = React;
-
-render(<AppComponent />, document.getElementById("app"));
--- test/tape-spec.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape';
-
-test('A passing test', (assert) => {
-
-  assert.pass('This test will pass.');
-
-  assert.end();
-});
