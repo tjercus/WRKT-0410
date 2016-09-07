0cde3bee585cda624fdaafba33db270e5aa3663a
tjerk valentijn <tjerk.valentijn@gmail.com>
2016-09-07 14:31:29 +0200

    rest-api supports loading multiple plans

--- rest-api.js
+++ rest-api.js
@@ -1,41 +1,47 @@
-var restify = require('restify'),
-  fs = require('fs'),
+var restify = require("restify"),
+  fs = require("fs"),
   url = require("url");
 
 var server = restify.createServer();
 
 server.use(restify.CORS({}));
-server.opts('/\.*/', corsHandler, optionsRoute);
+server.opts("/\.*/", corsHandler, optionsRoute);
 
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.get("/trainings", restify.serveStatic({
-  directory: './data',
+  directory: "./data",
   file: "trainings.js"
 }));
 
-server.get("/traininginstances", restify.serveStatic({
-  directory: './data',
-  file: "traininginstances.js"
-}));
+server.get("/traininginstances/:uuid", function(req, res, next) {
+  res.setHeader("Content-Type", "application/text");
+  fs.readFile("./data/traininginstances_" + req.params.uuid + ".js", "utf8", (err, contents) => {
+    res.send(200, contents);
+  });
+  return next();
+});
 
-server.get("/plans", restify.serveStatic({
-  directory: './data',
-  file: "plans.js"
-}));
+server.get("/plans/:uuid", function(req, res, next) {
+  res.setHeader("Content-Type", "application/text");
+  fs.readFile("./data/plan_" + req.params.uuid + ".js", "utf8", (err, contents) => {
+    res.send(200, contents);
+  });
+  return next();
+});
 
-server.put('/trainings', function(req, res, next) {
+server.put("/trainings", function(req, res, next) {
   writeFileFromRequestbody("./data/trainings.js", req, res);
   return next();
 });
 
-server.put('/plans', function(req, res, next) {  
+server.put("/plans", function(req, res, next) {  
   writeFileFromRequestbody("./data/plans.js", req, res);
   return next();
 });
 
-server.put('/traininginstances', function(req, res, next) {
+server.put("/traininginstances", function(req, res, next) {
   writeFileFromRequestbody("./data/traininginstances.js", req, res);
   return next();
 });
@@ -47,7 +53,7 @@ function writeFileFromRequestbody(filename, req, res) {
       res.send(err);
     } else {
       res.writeHead(200, {
-        'Content-Type': 'application/json; charset=utf-8'
+        "Content-Type": "application/json; charset=utf-8"
       });
       res.end();
       console.log(`saved ${filename} @ ${new Date()}`);
@@ -56,11 +62,11 @@ function writeFileFromRequestbody(filename, req, res) {
 };
 
 function corsHandler(req, res, next) {  
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-  res.setHeader('Access-Control-Max-Age', '1000');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader("Access-Control-Expose-Headers", "X-Api-Version, X-Request-Id, X-Response-Time");
+  res.setHeader("Access-Control-Max-Age", "1000");
   return next();
 }
 
