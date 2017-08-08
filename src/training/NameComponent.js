cf56e69d9c6b9a31989595eecad5b5e5a8db1730
tjercus <tjerk.valentijn@gmail.com>
2017-08-08 15:09:45 +0200

    added NameComponent

---
+++ src/training/NameComponent.js
@@ -0,0 +1,15 @@
+
+export default class NameComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // console.log(`SegmentView props.uuid ${JSON.stringify(props.uuid)}`);
+
+    this.state = {}; // TODO set empty component?
+  }
+
+  render() {
+
+  }
+
+}
