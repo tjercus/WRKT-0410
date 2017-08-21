220e4ce7be0a6d21dda40bd9012e24defae33bcf
tjercus <tjerk.valentijn@gmail.com>
2017-08-21 15:08:20 +0200

    new plan for amsterdam 2017

---
+++ data/plan_f80cf9c3-ba6d-4bac-8eba-f8b9b0749a51.js
@@ -0,0 +1,99 @@
+{
+	"uuid": "f80cf9c3-ba6d-4bac-8eba-f8b9b0749a51",
+	"name": "Amsterdam 2017",
+	"days": [
+		{
+			"uuid": "c7c68b44-1e29-4a16-86e0-ea29a0bf63c0",
+			"trainings": [
+				{
+					"instanceId": "f9f8b0c7-9e8c-48d9-901f-f631bb5e62d2"
+				}
+			]
+		},
+		{
+			"uuid": "fc8d3aef-2ec3-445b-bfdf-4fd3babf3e38",
+			"trainings": [
+				{
+					"instanceId": "4950767d-3ec7-4051-b544-9b64feef1fe3"
+				}
+			]
+		},
+		{
+			"uuid": "03cd518a-812e-4067-942b-3c3c6268572c",
+			"trainings": [
+				{
+					"instanceId": "a63dc934-9bc6-4659-8819-278cbd3c2d7e"
+				}
+			]
+		},
+		{
+			"uuid": "3743b24f-11b2-4638-8e82-c56993032e7b",
+			"trainings": [
+				{
+					"instanceId": "37f517ee-677e-4530-a369-b39a69b17a6f"
+				},
+				{
+					"instanceId": "a2e27138-474d-404f-915b-b85a4bd12693"
+				}
+			]
+		},
+		{
+			"uuid": "4cdaa728-d2e6-4a50-9922-c7115485dc4b",
+			"trainings": [
+				{
+					"instanceId": "96747c3c-2642-49d0-a557-3ea728e21a10"
+				},
+				{
+					"instanceId": "7f31aa20-8add-488b-81b7-38b3daeaa9c3"
+				}
+			]
+		},
+		{
+			"uuid": "9d75cdef-cbbb-4461-a431-e7dd16ab55c9",
+			"trainings": [
+				{
+					"instanceId": "ff1e2809-4df4-4329-a2b6-718d3504bee3"
+				},
+				{
+					"instanceId": "a99751b6-44b0-4619-b0d9-d8347436a6af"
+				}
+			]
+		},
+		{
+			"uuid": "fbc6d7b4-9036-43aa-b977-68a7546b6255",
+			"trainings": [
+				{
+					"instanceId": "92b49597-1250-43a8-b16d-a4d2f45f340f"
+				}
+			]
+		},
+		{
+			"uuid": "e876ba91-9080-4ac4-b967-d52f547c00bd",
+			"trainings": [
+				{
+					"instanceId": "9795697c-8b03-49bf-bd7d-8b4931326d2c"
+				}
+			]
+		},
+		{
+			"uuid": "c655d2e7-70a6-4c11-85a4-c7b883bb5b98",
+			"trainings": [
+				{
+					"instanceId": "328f1ca2-cfa2-4840-8395-2ac0427294bb"
+				}
+			]
+		},
+		{
+			"uuid": "52d77ecd-dac5-45a9-9c33-78755e373ca9",
+			"trainings": [
+				{
+					"instanceId": "67c381a3-e459-4a2e-9fca-c5f3e0af35d9"
+				},
+				{
+					"instanceId": "6f4e528e-9cc8-4312-bd79-31c8c487823d"
+				}
+			]
+		}
+	],
+	"startDate": "2017-08-13"
+}
