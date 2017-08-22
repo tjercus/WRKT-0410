ba1cf8d9071b02c9d2b67a2bbfbda5dacd1f887d
tjercus <tjerk.valentijn@gmail.com>
2017-08-22 11:58:00 +0200

    added Testplan and implemented REMOVE_INSTANCE_CMD

---
+++ data/plan_94d0c35d-4463-4796-8a4b-30730802e229.js
@@ -0,0 +1,15 @@
+{
+	"uuid": "94d0c35d-4463-4796-8a4b-30730802e229",
+	"name": "Testplan",
+	"days": [
+		{
+			"uuid": "cc45be68-8c2f-4175-a56b-905f4817e36a",
+			"trainings": [
+				{
+					"instanceId": "dca44d37-7ac3-4bf9-96d7-333a168eb851"
+				}
+			]
+		}
+	],
+	"startDate": "2016-01-01"
+}
---
+++ data/traininginstances_94d0c35d-4463-4796-8a4b-30730802e229.js
@@ -0,0 +1,29 @@
+[
+	{
+		"uuid": "dca44d37-7ac3-4bf9-96d7-333a168eb851",
+		"name": "42.2k @MP",
+		"type": "workout",
+		"segments": [
+			{
+				"uuid": "47",
+				"distance": "42.2",
+				"duration": "02:55:50",
+				"pace": "04:10"
+			}
+		]
+	},
+	{
+		"uuid": "b36fb2be-a03d-495b-8b28-ead8601c49f5",
+		"name": "gf4567457",
+		"type": "workout",
+		"segments": [
+			{
+				"uuid": "f6a64072-1a67-458f-962b-5a66704379b7",
+				"distance": 0,
+				"duration": "00:00:00",
+				"pace": "00:00"
+			}
+		],
+		"isNameEditable": false
+	}
+]
